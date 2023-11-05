9f2f404a98d5167afbbd571d4b295663e2ed5de4
iTrooz <hey@itrooz.fr>
2023-11-05 22:42:03 +0100

    remove useless lines from triplet

--- crosscompile/arm-osx-mytriplet.cmake
+++ crosscompile/arm-osx-mytriplet.cmake
@@ -1,10 +1,6 @@
 set(VCPKG_TARGET_ARCHITECTURE arm64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
-# You can override linkage type by package if needed
-if(PORT STREQUAL "portaudio")
-    set(VCPKG_LIBRARY_LINKAGE dynamic)
-endif()
 
 set(VCPKG_BUILD_TYPE release)
 
